e5b92bef7ca93cbc98cf1b5dd9ee14d4f104c369
TheLuis <luisdavidgilcabarcas47@gmail.com>
2025-11-14 12:15:45 -0500

    Desarrollo completo del proyecto

--- public/js/main.js
+++ public/js/main.js
@@ -36,4 +36,83 @@ document.addEventListener('DOMContentLoaded', () => {
 
   el.textContent = '';
   loop();
+
+/* Animaciones implementadas con scrollReveal */
+const sr = ScrollReveal({
+    reset: true,         
+    distance: "60px",    
+    duration: 1500,       
+    easing: "cubic-bezier(0.19, 0.75, 0.75, 1)", 
+    opacity: 0
+});
+
+sr.reveal(".container-header", {
+    origin: "top"
+});
+
+sr.reveal(".profile .me", {
+    origin: "left"
+});
+
+sr.reveal(".profile h1", {
+    origin: "bottom",
+    delay: 200
+});
+
+sr.reveal(".profile p", {
+    origin: "bottom",
+    delay: 350
+});
+
+sr.reveal(".container-contact", {
+    origin: "bottom",
+    delay: 500
+});
+
+sr.reveal(".title-experience", {
+    origin: "left",
+});
+
+sr.reveal(".timeline-item.left", {
+    origin: "left",
+    delay: 200
+});
+
+sr.reveal(".timeline-item.right", {
+    origin: "right",
+    delay: 300
+});
+
+sr.reveal(".title-me", {
+    origin: "left",
+});
+
+sr.reveal(".description-me", {
+    origin: "bottom",
+    delay: 250
+});
+
+sr.reveal(".img-hero", {
+    origin: "right",
+    delay: 300
+});
+
+sr.reveal(".title-education", {
+    origin: "left",
+});
+
+sr.reveal(".education", {
+    origin: "bottom",
+    delay: 200
+});
+
+sr.reveal(".title-technologies", {
+    origin: "left",
+});
+
+sr.reveal(".technologies", {
+    origin: "bottom",
+    interval: 200 
+});
+  
 });
